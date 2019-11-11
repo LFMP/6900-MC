7bcb6c1ff0b4780208e75d367ec5311285e16c70
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-11-11 16:41:15 -0300

    change makefile to test in main

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 // #include "include/sen.h"
 // #include "include/coss.h"
-#include "include/e_x.h"
+//#include "include/e_x.h"
+#include "include/lnd.h"
 
 void main(){
   ieee754 res;
-  printf("e^x:\n");
-  e(2, &res);
+  ieee754 e = double_to_ieee754(5);
+  printf("ln:\n");
+  lnd(&e, &res);
   print_ieee754(res);
   printf("%lg\n",ieee754_to_double(res));
   
