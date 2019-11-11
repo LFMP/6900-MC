ea8977f49fadc669b813cddb80403378a191c093
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-11-11 15:39:55 -0300

    fix série e constante log2 de e

--- src/e_x.c
+++ src/e_x.c
@@ -1,11 +1,11 @@
 #include "include/e_x.h"
 
 double calc_e(double x){
-  return (1 + x + pow(2,fatorial02) + pow(3,fatorial03) + pow(4,fatorial04) + pow(5,fatorial05) + pow(6,fatorial06) + pow(7,fatorial07) + pow(8,fatorial08) + pow(9,fatorial09) + pow(10,fatorial10) + pow(11,fatorial11) + pow(12,fatorial12) + pow(13,fatorial13) + pow(14,fatorial14) + pow(15,fatorial15));
+  return (1 + x + (pow(x,2)/fatorial02) + (pow(x,3)/fatorial03) + (pow(x,4)/fatorial04) + (pow(x,5)/fatorial05) + (pow(x,6)/fatorial06) + (pow(x,7)/fatorial07) + (pow(x,8)/fatorial08) + (pow(x,9)/fatorial09) + (pow(x,10)/fatorial10) + (pow(x,11)/fatorial11) + (pow(x,12)/fatorial12) + (pow(x,13)/fatorial13) + (pow(x,14)/fatorial14) + (pow(x,15)/fatorial15));
 }
 void e(double x,ieee754 *res){
-  double coef = log2(E);
+  double coef = log2_e;
   double k = (coef - x) / (-coef);
-  double x1 = x - k * coef;
+  double x1 = x - k * coef;           
   *res = double_to_ieee754(pow(2,k) * calc_e(x1));
 }
