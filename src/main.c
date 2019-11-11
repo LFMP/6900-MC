7146cfc71ec0c3d9e30cf41c8bffe4602fbe56e2
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-11-11 15:40:07 -0300

    test e^x

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
-#include "include/sen.h"
+// #include "include/sen.h"
+// #include "include/coss.h"
+#include "include/e_x.h"
 
 void main(){
   ieee754 res;
-  sen(PI_sobre_2, &res);
+  printf("e^x:\n");
+  e(2, &res);
   print_ieee754(res);
+  printf("%lg\n",ieee754_to_double(res));
+  
+  // res = double_to_ieee754(1451);
+  // print_ieee754(res);
+  // printf("%g\n",ieee754_to_double(res));
 }
