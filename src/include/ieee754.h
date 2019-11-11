2b5e1a330887e13f031add79bf3ba23a02e4031f
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-11-11 15:39:23 -0300

    refactor

--- src/include/ieee754.h
+++ src/include/ieee754.h
@@ -7,16 +7,17 @@
 #define PI 3.141592653589793
 #define PI_sobre_2 1.5707963267948966
 #define E 2.718281828459045
+#define log2_e 0.6931471805599453
 
 typedef struct ieee754{
   int sign;
   int exp;
   double signif;
 } ieee754;
+#endif
 
 ieee754 double_to_ieee754(double x);
 
 double ieee754_to_double(ieee754 number);
 
 void print_ieee754(ieee754 number);
-#endif
