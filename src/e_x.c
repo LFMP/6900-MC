45462f43568b8cc6e263f3bcb6a93afad775568b
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-11-11 14:41:19 -0300

    update e^x to void

--- src/e_x.c
+++ src/e_x.c
@@ -3,9 +3,9 @@
 double calc_e(double x){
   return (1 + x + pow(2,fatorial02) + pow(3,fatorial03) + pow(4,fatorial04) + pow(5,fatorial05) + pow(6,fatorial06) + pow(7,fatorial07) + pow(8,fatorial08) + pow(9,fatorial09) + pow(10,fatorial10) + pow(11,fatorial11) + pow(12,fatorial12) + pow(13,fatorial13) + pow(14,fatorial14) + pow(15,fatorial15));
 }
-ieee754 e(double x){
+void e(double x,ieee754 *res){
   double coef = log2(E);
   double k = (coef - x) / (-coef);
   double x1 = x - k * coef;
-  return double_to_ieee754(pow(2,k) * calc_e(x1));
+  *res = double_to_ieee754(pow(2,k) * calc_e(x1));
 }
--- src/include/e_x.h
+++ src/include/e_x.h
@@ -3,5 +3,5 @@
 #include "ieee754.h"
 #include <math.h>
 double calc_e(double x);
-ieee754 e(double x);
+void e(double x,ieee754 *res);
 #endif
