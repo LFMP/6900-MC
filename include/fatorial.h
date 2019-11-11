1c949b41e4cd8d47bf9d8e6af28a2cf11d5e1c99
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-11-11 09:40:40 -0300

    add fatorial cons

---
+++ include/fatorial.h
@@ -0,0 +1,16 @@
+#ifndef fatorial
+#define fatorial02 2
+#define fatorial03 6
+#define fatorial04 24
+#define fatorial05 120
+#define fatorial06 720
+#define fatorial07 5040
+#define fatorial08 40320
+#define fatorial09 362880
+#define fatorial10 3628800
+#define fatorial11 39916800
+#define fatorial12 479001600
+#define fatorial13 6227020800
+#define fatorial14 87178291200
+#define fatorial15 1307674368000
+#endif
